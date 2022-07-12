fe419218f85109ba4604cd76bca2ed09f5d344d3
Jock1323 <mirsidiq2002@gmail.com>
2022-07-12 13:30:46 +0500

    finish the exam task add pagination

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let elLogOutBtn = document.querySelector('.log-out');
 let elForm = document.querySelector('.form-search');
 let elSearchInput = document.querySelector('.search-input');
 let elSearchBtn = document.querySelector('.search-btn');
-let elOrderByNew = document.querySelector('.order-by');
+let elOrderByNew = document.querySelector('.results__order');
 let elList = document.querySelector('.main-card');
 let elModal = document.querySelector('.more-info__modal');
 let elOverlay = document.querySelector('.more-info__overlay');
@@ -12,10 +12,25 @@ let elTable = document.querySelector('.main-left__table');
 let elPrevBtn=document.querySelector('.prev');
 let elNextBtn=document.querySelector('.next')
 let elNumbers=document.querySelectorAll('.numbers .numbers__link');
-let bookName='flowers'
+let elResultsNum=document.querySelector('.results__num');
+let elPagination=document.querySelector('.numbers');
+let elPageBtnWrapper=document.querySelector('.pagination')
+let elErrorMessage=document.querySelector('.error');
+let orderName='relevance';
+let bookName='python'
+let parsedData=JSON.parse(window.localStorage.getItem('bookmarks'));
 let fullArr = [];
-let bookmarkArr = [];
+let bookmarkArr;
 let page=0;
+if(parsedData){
+    bookmarkArr=parsedData;
+}
+else{
+    bookmarkArr=[];
+    console.log(bookmarkArr);
+}
+elResultsNum.textContent=fullArr.totalItems;
+
 
 //reset Links
 
@@ -31,25 +46,25 @@ let renderCards = (fullArr, htmlElement) => {
         let item = `
     <li class="main-card__item">
     <div class="card-header main-card__img">
-        <img src="${element.volumeInfo.imageLinks.thumbnail}" width="200" height="200" alt="an image">
+        <img src="${element.volumeInfo.imageLinks?.thumbnail}" width="200" height="200" alt="an image">
     </div>
     <div class="card-body">
         <h4 class="card-title main-card__title">
-        ${element.volumeInfo.title}
+        ${element.volumeInfo?.title}
         </h4>
         <p class="main-card__text">
-            ${element.volumeInfo.authors}
+            ${element.volumeInfo?.authors}
         </p>
-        <span class="main-card__year">${element.volumeInfo.publishedDate}</span>
+        <span class="main-card__year">${element.volumeInfo?.publishedDate}</span>
         <div class="main-card__btns">
-            <button class=" main-card__bookmark" data-bookmark=${element.id}>
+            <button class=" main-card__bookmark" data-bookmark=${element?.id}>
                 Bookmark
             </button>
-            <button class=" main-card__info" data-more=${element.id}>
+            <button class=" main-card__info" data-more=${element?.id}>
                 More Info
             </button>
-            <a href='${element.volumeInfo.previewLink}' class="main-card__read"
-                data-read=${element.id}>
+            <a href='${element.volumeInfo?.previewLink}' class="main-card__read"
+                data-read=${element?.id}>
                 Read
             </a>
         </div>
@@ -81,7 +96,7 @@ let renderMoreInfoModal = (item, htmlElement) => {
     let moreInfoCategories=document.createElement('p');
     let moreInfoPage=document.createElement('p');
     let moreInfoBtnWrapper=document.createElement('div');
-    let moreInfoBtn=document.createElement('button');
+    let moreInfoBtn=document.createElement('a');
     let moreInfoPublishedWrapper=document.createElement('span')
     let moreInfoPublishersWrapper=document.createElement('span')
     let moreInfoPageWrapper=document.createElement('span');
@@ -90,8 +105,9 @@ let renderMoreInfoModal = (item, htmlElement) => {
     moreInfoTitle.setAttribute('class','more-info__title');
     moreInfoXmark.setAttribute('class','x-mark')
     moreInfoXmark.setAttribute('src','./images/x-mark.svg');
-    moreInfoImg.setAttribute('src',item.volumeInfo.imageLinks.thumbnail)
+    moreInfoImg.setAttribute('src',item.volumeInfo.imageLinks?.thumbnail)
     moreInfoImg.setAttribute('class','more-info__img')
+    moreInfoImg.setAttribute('alt','something went wrong')
     moreInfoDesc.setAttribute('class','more-info__text')
     moreInfoAuthors.setAttribute('class','more-info__author')
     moreInfoPublished.setAttribute('class','more-info__author');
@@ -99,10 +115,11 @@ let renderMoreInfoModal = (item, htmlElement) => {
     moreInfoCategories.setAttribute('class','more-info__author');
     moreInfoPage.setAttribute('class','more-info__author');
     moreInfoBtn.setAttribute('class','more-info__btn')
+    moreInfoBtn.setAttribute('href',item.volumeInfo.previewLink)
     moreInfoBtnWrapper.setAttribute('class','more-info__btn-wrapper')
 
-    moreInfoTitle.textContent=item.volumeInfo.title;
-    moreInfoDesc.textContent=item.volumeInfo.description;
+    moreInfoTitle.textContent=item.volumeInfo?.title;
+    moreInfoDesc.textContent=item.volumeInfo?.description;
 
     moreInfoAuthors.textContent="Authors :";
     moreInfoPublished.textContent="Published :";
@@ -110,30 +127,31 @@ let renderMoreInfoModal = (item, htmlElement) => {
     moreInfoCategories.textContent="Categories :";
     moreInfoPage.textContent="Pages Count :"
     moreInfoBtn.textContent="Read"
-    item.volumeInfo.authors.forEach(element=>{
+    item.volumeInfo.authors?.forEach(element=>{
         let name=document.createElement('span');
         name.setAttribute('class','more-info__author-name')
         name.textContent=element;
         moreInfoAuthors.appendChild(name)
     })
     
-    item.volumeInfo.categories.forEach(element=>{
+    item.volumeInfo.categories?.forEach(element=>{
         let name=document.createElement('span');
         name.setAttribute('class','more-info__author-name')
         name.textContent=element;
         moreInfoCategories.appendChild(name)
     })
     moreInfoPublishedWrapper.setAttribute('class','more-info__author-name')
-    moreInfoPublishedWrapper.textContent=item.volumeInfo.publishedDate;
+    moreInfoPublishedWrapper.textContent=item.volumeInfo?.publishedDate;
     moreInfoPublished.appendChild(moreInfoPublishedWrapper)
 
     moreInfoPublishersWrapper.setAttribute('class','more-info__author-name')
-    moreInfoPublishersWrapper.textContent=item.volumeInfo.publisher;
+    moreInfoPublishersWrapper.textContent=item.volumeInfo?.publisher;
     moreInfoPublishers.appendChild(moreInfoPublishersWrapper);
 
     moreInfoPageWrapper.setAttribute('class','more-info__author-name');
-    moreInfoPageWrapper.textContent=item.volumeInfo.pageCount;
+    moreInfoPageWrapper.textContent=item.volumeInfo?.pageCount;
     moreInfoPage.appendChild(moreInfoPageWrapper)
+
 
     htmlElement.appendChild(moreInfoFlex);
     moreInfoFlex.append(moreInfoTitle)
@@ -150,28 +168,64 @@ let renderMoreInfoModal = (item, htmlElement) => {
 
 }
 //render Bookmark
-
 let renderBookmark = (arr, htmlElement) => {
     let txt = '';
+    
     arr.forEach(item => {
         let element = `
         <div class="main-left__table-row">
                         <div class="main-left__table-data  d-flex justify-content-between align-items-center">
                             <h4 class="book-title">
-                                ${item.volumeInfo.title}
+                                ${item.volumeInfo?.title}
                             </h4>
-                            <p class="book-text">${item.volumeInfo.authors}</p>
+                            <p class="book-text">${item.volumeInfo?.authors}</p>
                         </div>
                         <div class="main-left__table-icons">
-                            <img src="./images/book-open.svg" alt="book open" width="24" height="24">
+                            <a href="${item.volumeInfo?.previewLink}">
+                            <img src="./images/book-open.svg" class="bookmark-read" alt="book open" width="24" height="24" data-read='${item?.id}'>
+                            </a>
                             <img src="./images/delete.svg" class=" bookmark-delete" alt='delete icon' width="24"
-                                height="24" data-delete='${item.id}'>
+                                height="24" data-delete='${item?.id}'>
                         </div>
                     </div>
         `
         txt += element;
     })
     htmlElement.innerHTML = txt;
+}
+
+//  render Pagination
+
+let res;
+let renderPage=(data)=>{
+    res=0;
+    let txt='';
+    if(Number(data)%10===0){
+        res=Math.floor(data/10);
+    }
+    else{
+        res=Math.floor(data/10)+1;
+    }
+    for(let item=0;item<=res;item++){
+        let page=`
+        <li class="number"><a href="#" class="numbers__link" data-pageid=${item}>${item+1}</a></li>
+        `
+        txt+=page;
+    }
+    elPagination.innerHTML=txt;
+   
+}
+//renderError
+
+let renderError=()=>{
+    elErrorMessage.classList.add('error-active');
+    elList.innerHTML="";
+    elPagination.innerHTML="";
+    elPrevBtn.classList.add('prev--active');
+    elPrevBtn.disabled=true;
+    elNextBtn.classList.add('prev--active');
+    elNextBtn.disabled=true;
+    page=0;
 }
 
 if (!token) {
@@ -184,9 +238,16 @@ elLogOutBtn.addEventListener('click', () => {
 
 elForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    elErrorMessage.classList.remove('error-active');
     bookName = elSearchInput.value;
+    orderName='relevance'
     fullData();
     elSearchInput.value = null;
+})
+elOrderByNew.addEventListener('click',()=>{
+    orderName='newest';
+    elErrorMessage.classList.remove('error-active');
+    fullData();
 })
 elList.addEventListener('click', e => {
     if (e.target.matches('.main-card__info')) {
@@ -199,6 +260,7 @@ elList.addEventListener('click', e => {
         let bookmarkId = e.target.dataset.bookmark;
         let findElement = fullArr.find(item => item.id === bookmarkId);
         if (!bookmarkArr.includes(findElement)) bookmarkArr.push(findElement);
+        window.localStorage.setItem('bookmarks',JSON.stringify(bookmarkArr));
         renderBookmark(bookmarkArr, elTable);
     }
 })
@@ -208,7 +270,16 @@ elTable.addEventListener('click',e=>{
         let bookmarkDeleteId=e.target.dataset.delete;
         let findDeletedIndex=bookmarkArr.findIndex(item=>item.id===bookmarkDeleteId);
         bookmarkArr.splice(findDeletedIndex,1);
+        window.localStorage.setItem('bookmarks',JSON.stringify(bookmarkArr));
+        if(bookmarkArr.length===0){
+            window.localStorage.removeItem('bookmarks');
+        }
         renderBookmark(bookmarkArr,elTable);
+    }
+})
+elModal.addEventListener('click',(e)=>{
+    if(e.target.matches('.x-mark')){
+        closeModal();
     }
 })
 elOverlay.addEventListener('click', closeModal)
@@ -218,43 +289,67 @@ document.addEventListener('keydown', (e) => {
         elOverlay.classList.remove('more-info__overlay--active')
     }
 })
+elPagination.addEventListener('click',e=>{
+    if(e.target.matches('.numbers__link')){
+        let pageBtnId=e.target.dataset.pageid*1;
+        page=pageBtnId*10;
+        if(page>0){
+            elNextBtn.classList.remove('prev--active');
+            elNextBtn.disabled=false;
+            elPrevBtn.classList.remove('prev--active');
+            elPrevBtn.disabled=false;
+        }
+        else{
+            elPrevBtn.classList.add('prev--active');
+            elPrevBtn.disabled=true;
+        }
+        fullData();
+    }
+})
 elPrevBtn.addEventListener('click',(e)=>{
     elNextBtn.classList.remove('prev--active');
     elNextBtn.disabled=false;
-    if(page>1){
-        page--;
+    if(page>10){
+        page-=10;
+        fullData()
     }
     else{
+        page=0;
         elPrevBtn.classList.add('prev--active')
         elPrevBtn.disabled=true;
-        page=0;
+        fullData()
     }
-    console.log(page);
 })
 elNextBtn.addEventListener('click',()=>{
     elPrevBtn.classList.remove('prev--active')
     elPrevBtn.disabled=false;
-    page++
-    if(page===5){
+    page+=10;
+    if(page===res*10){
         elNextBtn.classList.add('prev--active');
         elNextBtn.disabled=true;
-        page=5;
+        page=res;
     }
-    console.log(page);
+    fullData()
 })
-elNumbers.forEach(item=>{
-    item.addEventListener('click',()=>{
-        resetLinks();
-        item.classList.add('active')
-    })
-})
+// elNumbers.forEach(item=>{
+//     item.addEventListener('click',()=>{
+//         resetLinks();
+//         item.classList.add('active')
+//     })
+// })
 const fullData = () => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookName}`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookName}&orderBy=${orderName}&startIndex=${page}`)
         .then(req => req.json())
         .then(data => {
             fullArr = data.items;
+            elResultsNum.textContent=data.totalItems
             console.log(data);
+            renderPage(data.totalItems);
             renderCards(data.items, elList)
         })
+        .catch(err=>{
+            renderError();
+        })
 }
+renderBookmark(bookmarkArr,elTable);
 fullData();
